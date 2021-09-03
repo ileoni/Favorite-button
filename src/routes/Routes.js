0e8a9a994886a9f43eeea2827612146f42e04263
ileoni <higorleonii@gmail.com>
2021-09-03 20:02:39 -0300

    adicionando arquivo de configuração routes

---
+++ src/routes/Routes.js
@@ -0,0 +1,23 @@
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom'
+
+import {Home} from '../components/Home'
+
+const Routes = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route
+                    exact
+                    path="/"
+                    component={Home}
+                />
+            </Switch>
+        </Router>
+    )
+}
+
+export default Routes
