c2d565b45a48493739073f145de76acb9b87397d
ileoni <higorleonii@gmail.com>
2021-09-04 16:40:35 -0300

    modificando arquivo App, Favorite e index

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-
 import {Main} from './components/main/Main'
 
 function App() {
--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -1,10 +1,58 @@
 import './Favorite.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {
+    useSelector,
+    useDispatch
+} from 'react-redux'
+import { isToggleOn } from './favoriteSlice'
+import { Icons } from './Icons'
 
 export const Favorite = () => {
+    const favorites = useSelector(state => state.favorites)
+    const dispatch = useDispatch()
+    
     return (
-        <div className="favorite">
-            
-        </div>
+        <>
+            <div className="favorite">
+                <h1>
+                    Icons
+                </h1>
+                <div className="container">
+                    {
+                        favorites.map( (favorite, key) => {
+                            return (
+                                <div className="card" key={key}>
+                                    <button className={`btn ${favorite.toggle && (favorite.color)}`} onClick={() => dispatch(isToggleOn(favorite))}>
+                                        <span>
+                                            {
+                                                favorite.name
+                                            }
+                                        </span>
+                                        <i>
+                                            {
+                                                !favorite.toggle && (
+                                                    <Icons type={favorite.icons.before}/>
+                                                )
+                                            }
+                                            {
+                                                favorite.toggle && (
+                                                    <Icons type={favorite.icons.after}/>
+                                                )
+                                            }
+                                        </i>
+                                    </button>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <div>
+                <Link to="/">
+                    Go to Home!
+                </Link>
+            </div>
+        </>
     )
 }
