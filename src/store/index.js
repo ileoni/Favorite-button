e8d9b763ec6924c791be21772cc93cb57bb34d1f
ileoni <higorleonii@gmail.com>
2021-09-05 16:55:31 -0300

    modificação store, e configurando redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,31 @@
 import {configureStore} from '@reduxjs/toolkit'
 import favoriteReducer from '../components/favoriteSlice'
+import {
+    persistStore,
+    persistReducer,
+    PERSIST
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, favoriteReducer)
 
 let store = configureStore({
     reducer: {
-        favorites: favoriteReducer 
-    }
+        favorites: persistedReducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [PERSIST]
+        }
+    })
 })
+
+export let persistor = persistStore(store)  
 
 export default store
